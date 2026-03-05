33effbb0ff1bbd929e066a34973e1cfffbc0ba1f
Claire Rosemond <claire.rosemond@noaa.gov>
2026-03-05 11:47:54 -0800

    add home tab and explanatory text

--- ui.R
+++ ui.R
@@ -7,10 +7,20 @@ ui <- shiny::fluidPage(
   
   shiny::tabsetPanel(
     
+    tabPanel("Home",
+      # Add text using HTML formatting functions
+       h1("About the tool and data"), # Creates a primary header
+          p("This tool facilitates collaborative review of the NOAA NWFSC FRAM FRS survey sampling protocols and data collection targets designed to support stock assessments and research. The data are from the NOAA NWFSC West Coast Groundfish Bottomtrawl Survey (WCGBTS) and NOAA NWFSC Hook and Line Survey (HKLS). The data are filtered to include the species and stock areas currently included (or considered for inclusion) in the PFMC Fishery Managment Plan."), # Creates a paragraph of text
+            "To view the code and to download the data tables, visit the",
+            a("GitHub repository.", href = "https://github.com/rclairer/survey_data_prioritization/") # Inserts a hyperlink
+      ), 
+    
     shiny::tabPanel(
       "WCGBTS length and age structure tallies",
         shiny::sidebarLayout(
         shiny::sidebarPanel(
+          
+      p("Select the data type (length or age structure tallies, year range, and species to display."),
       
       shiny::checkboxGroupInput(
         "table_type",
@@ -44,7 +54,8 @@ ui <- shiny::fluidPage(
               choices = c("2025 catch per tow tallies" = "tows_targets",
                           "Average (across all years) catch per tow tallies" = "tows_targets_average"),
               selected = "tows_targets"
-            )
+            ),
+            p("The highlighted cells correspond to the set length targets.")
           ),
           shiny::mainPanel(
         gt::gt_output("tows_targets_table")
@@ -56,6 +67,8 @@ shiny::tabPanel(
   "HKLS length and age structure tallies",
   shiny::sidebarLayout(
     shiny::sidebarPanel(
+
+      p("Select the data type (length or age structure tallies, year range, and species to display."),
       
       shiny::checkboxGroupInput(
         "hkls_table_type",
